3c446e3ca83fceb77524c7ea4c4688f33583f811
breadboi <brett.carney1273@gmail.com>
2019-11-05 16:05:21 -0600

    Created the basic layout for the Quote Website

---
+++ resources/library/azure.php
@@ -0,0 +1,25 @@
+<?php
+// File responsible for setting up our DEV database PDO
+
+// Include our configuration file
+require_once("/resources/config.php");
+
+// Get our connection info from the config file
+$dbname = $config['db']['dev']['dbname'];
+$username = $config['db']['dev']['username'];
+$password = $config['db']['dev']['password'];
+$host = $config['db']['dev']['host'];
+
+// PHP Data Objects(PDO)
+try 
+{
+    $devPDO = new PDO("sqlsrv:server = tcp:{$host},1433; Database = {$dbname}", "{$username}", "{$password}");
+    $devPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) 
+{
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+?>
