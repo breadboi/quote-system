43d4c59fc7163cf60e1dfe5ff9a6288b819cbe93
breadboi <brett.carney1273@gmail.com>
2019-11-23 19:36:53 -0600

    Separated code into multiple files for better organization and prepared for modal population

--- public_html/Admin_System/adminIndex.php
+++ public_html/Admin_System/adminIndex.php
@@ -19,6 +19,7 @@
 
 <body>
 
+    <!-- Search Form -->
     <form method="POST" name="adminsearch">
 
         <!-- Search Type -->
@@ -60,69 +61,13 @@
         <button type="submit" class="btn btn-primary">Search</button>
     </form>
 
-
+    <!-- Table Loader -->
     <?php
-        // Load config and functions
-        require_once("../../resources/config.php");
-        include("../../resources/library/devDatabase.php");
-        require_once("../../resources/library/tableformat.php");
-
-        if (isset($_POST["searchChoice"]))
-        {
-            $searchChoice = $_POST["searchChoice"];            
-            $sql = "";
-
-            // Set sql string
-            switch($searchChoice)
-            {
-                case 0:
-                    $sql = "SELECT id AS ID, name AS Name, accumulated_commission AS 'Total Commission', address AS Address FROM sales_associates
-                            WHERE name LIKE CONCAT('%', :salesAssociateName, '%');";
-                    break;
-                case 1:
-                    $sql = "SELECT id AS ID, customer_name AS Name, contact AS Contact, street AS Street, city AS City, secret_notes AS Notes, discount AS Discount
-                            WHERE customer_name LIKE CONCAT('%', :customerName, '%');";
-                    break;
-            }
-
-            // Prepare pdo
-            $prepared = $devPdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-
-            // Perform respective query
-            switch($searchChoice)
-            {
-                case 0:
-                    $salesAssociateName = $_POST["salesAssociateName"];
-                    $success = $prepared->execute(array(':salesAssociateName' => $salesAssociateName));
-
-                    if ($success)
-                    {
-                        $rows = $prepared->fetchAll(PDO::FETCH_ASSOC);
-                        tableHead($rows);
-                        tableBody($rows);
-                    }
-                    else
-                    {
-                        echo "<div class=\"failure\">An Error Occured... Please double check your search text</div>";
-                    }
-                break;
-                case 1:
-                    $customerName = $_POST["customerName"];
-                    $success = $prepared->execute(array(':customerName' => $customerName));
-
-                    if ($success)
-                    {
-                        $rows = $prepared->fetchAll(PDO::FETCH_ASSOC);
-                        tableHead($rows);
-                        tableBody($rows);
-                    }
-                    else
-                    {
-                        echo "<div class=\"failure\">An Error Occured... Please double check your search text</div>";
-                    }
-                break;
-            }
-        }
+        require_once("views/tableLoader.php");
+        
+        // Only show for the desired choice
+        if ($_POST["searchChoice"] == 0)
+            require_once("views/modalForm.html");
     ?>
 
 </body>
--- public_html/Admin_System/javascript/admin.js
+++ public_html/Admin_System/javascript/admin.js
@@ -30,3 +30,36 @@ function toggleQuoteFormItems(context) {
         $(".quoteFormItems").prop('required',true);
     }
 }
+
+$("td").on("click", function(e) { 
+    e.preventDefault();
+    //alert(this);
+
+    // Remove previous highlighting
+    $("tr").removeClass("cellselect");    
+
+    alert($(this).parents('tr')[0].text());
+
+    // Create row array variable
+    var rowArray = [];
+
+    // For each cell, we push to an array
+    $(this).parents('tr').find('td').each(function() {
+        // For each cell
+        rowArray.push($(this));
+    });
+
+    // Assign each row cell to a variable
+    var selectionId = rowArray[0];
+    var selectionName = rowArray[1];
+    var selectionCommission = rowArray[2];
+    var selectionAddress = rowArray[3];
+
+    $(this).parent().addClass("cellselect");
+
+    // set the field to the row value
+    $("#associateIdField").attr("value", selectionId);
+    $("#associateNameField").attr("value", selectionName);
+    $("#associateCommissionField").attr("value", selectionCommission);
+    $("#associateAddressField").attr("value", selectionAddress);
+});
